82f7f933f7fb93c053ce46db3354d6d934e62b71
tamires <tamireshc@gmail.com>
2022-12-14 19:41:55 -0300

    content first day finalize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    PrismaService,
+    // {
+    //   provide: MailService,
+    //   useClass: SMTPMailService, se for a partir de uma classe abstrata
+    // },
+  ],
 })
 export class AppModule {}
